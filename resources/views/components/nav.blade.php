3d60e21302c7096cbc44f286a9980b423563b634
omericoramos <omericoramos@gmail.com>
2024-04-04 17:06:23 -0300

    adicionando link dashboard no component nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,6 @@
 <nav>
     <ul class="flex">
+        <li class="mx-4 hover:text-rose-500 text-lg text-sky-600 font-bold"><a href="{{ route('dashboard') }}">Dashboard</a></li>
         <li class="mx-4 hover:text-rose-500 text-lg"><a href="{{ route('generatePdfView') }}" target="_blank">Visualizar pdf</a></li>
         <li class="mx-4 hover:text-rose-500 text-lg"><a href="{{ route('generatePdfSaveToStorage') }}">Salvar pdf na pasta storage</a></li>
         <li class="mx-4 hover:text-rose-500 text-lg"><a href="{{ route('generatePdfAndDownload') }}">Baixar pdf da lista</a></li>
--- resources/views/userListView.blade.php
+++ resources/views/userListView.blade.php
@@ -1,15 +1,4 @@
             <x-app-layout>
-                <x-slot name="header">
-                    <nav>
-                        <ul>
-                            <li><a href="{{ route('generatePdfView') }}" target="_blank">Visualizar pdf</a></li>
-                            <li><a href="{{ route('generatePdfSaveToStorage') }}">Salvar pdf na pasta storage</a></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </nav>
-                </x-slot>
-
                 <div class="py-12">
                     
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
